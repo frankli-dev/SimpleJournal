85e1139304134dc0e1d5b56316139c53c46c5b7b
Frank Li <frankli.consult@gmail.com>
2020-09-13 12:46:12 -0400

    User Experience: blink button

--- src/components/FeelingCard/index.js
+++ src/components/FeelingCard/index.js
@@ -6,29 +6,30 @@ import moment from 'moment'
 import './index.scss'
 
 const FeelingCard = ({ data: { rating: initialRating, text: initialText, month, day, saved }, onSave, theme, setTheme }) => {
-  const [rating, setRating] = useState(initialRating)
-  const [text, setText] = useState(initialText)
-  const touched = rating !== initialRating || text !== initialText
+  const [info, setInfo] = useState({
+    rating: initialRating,
+    text: initialText,
+    touched: false,
+  })
 
-  const savedClass = classnames('saved', { hide: touched || !saved })
+  const savedClass = classnames('saved', { hide: info.touched || !saved })
 
   useEffect(() => {
-    setRating(initialRating)
-    setText(initialText)
+    setInfo({ rating: initialRating, text: initialText, touched: false })
   }, [initialRating, initialText])
 
   useEffect(() => {
-    setTheme(rating)
-  }, [rating])
+    setTheme(info.rating)
+  }, [info.rating])
 
   return (
     <div className="feeling-card">
       <div className="feeling-card-header">
         <h2>How are you feeling today?</h2>
-        <RatingControl value={rating} onChange={(v) => setRating(v)} />
+        <RatingControl value={info.rating} onChange={(rating) => setInfo({ ...info, rating, touched: true })} />
       </div>
       <div className="feeling-card-body">
-        <textarea value={text} onChange={(e) => setText(e.target.value)} />
+        <textarea value={info.text} onChange={(e) => setInfo({ ...info, text: e.target.value, touched: true })} />
       </div>
       <div className="divider" />
       <div className="feeling-card-footer">
@@ -36,9 +37,11 @@ const FeelingCard = ({ data: { rating: initialRating, text: initialText, month, 
         <span className={savedClass}>Saved on {saved}</span>
 
         <SaveButton
-          hide={!touched || !rating || !text}
+          hide={!info.touched || !info.rating || !info.text}
           theme={theme}
-          onClick={() => onSave({ month, day, rating, text, saved: `${moment().format('MMMM DD')} at ${moment().format('m:ssa')}` })}
+          onClick={() =>
+            onSave({ month, day, rating: info.rating, text: info.text, saved: `${moment().format('MMMM DD')} at ${moment().format('m:ssa')}` })
+          }
         >
           Save
         </SaveButton>
