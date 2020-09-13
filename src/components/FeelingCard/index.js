b8b9d2b581a761323bfbdc8655f692830a67c50f
Frank Li <frankli.consult@gmail.com>
2020-09-13 01:12:42 -0400

    dynamic Button class

--- src/components/FeelingCard/index.js
+++ src/components/FeelingCard/index.js
@@ -1,23 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import RatingControl from '../RatingControl'
-import SaveButton from '../SaveButton'
+import React from 'react'
+import classnames from 'classnames'
+import { RatingControl, SaveButton } from 'components'
 import './index.scss'
 
-const FeelingCard = ({ data: { rating, text, date }, onSave, onChange }) => (
-  <div className="feeling-card">
-    <div className="feeling-card-header">
-      <h2>How are you feeling today?</h2>
-      <RatingControl value={rating} onChange={(v) => onChange('rating', v)} />
+const FeelingCard = ({ data: { rating, text, date }, onSave, onChange }) => {
+  const saveBtnClass = classnames(`fg-color-${rating}`, `border-color-${rating}`, { hide: !rating || !text })
+  return (
+    <div className="feeling-card">
+      <div className="feeling-card-header">
+        <h2>How are you feeling today?</h2>
+        <RatingControl value={rating} onChange={(v) => onChange('rating', v)} />
+      </div>
+      <div className="feeling-card-body">
+        <textarea defaultValue={text} onChange={(e) => onChange('text', e.target.value)} />
+      </div>
+      <div className="divider" />
+      <div className="feeling-card-footer">
+        <span className="date">{date}</span>
+        <SaveButton className={saveBtnClass} onClick={() => onSave({ date, rating, text })}>
+          Save
+        </SaveButton>
+      </div>
     </div>
-    <div className="feeling-card-body">
-      <textarea defaultValue={text} onChange={(e) => onChange('text', e.target.value)} />
-    </div>
-    <div className="divider" />
-    <div className="feeling-card-footer">
-      <span className="date">{date}</span>
-      <SaveButton onClick={() => onSave({ date, rating, text })}>Save</SaveButton>
-    </div>
-  </div>
-)
+  )
+}
 
 export default FeelingCard
--- src/components/SaveButton/index.js
+++ src/components/SaveButton/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import classnames from 'classnames'
 import './index.scss'
 
-const SaveButton = ({ children, onClick }) => {
+const SaveButton = ({ children, className, onClick }) => {
   return (
-    <button className="btn-save" onClick={onClick}>
+    <button className={classnames('btn-save', className)} onClick={onClick}>
       {children}
     </button>
   )
